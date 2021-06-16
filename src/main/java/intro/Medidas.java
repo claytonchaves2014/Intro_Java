d9458ef521058d975300a4aebd8f03ee0fcff851
cchaves <clayton.chaves@hotmail.com>
2021-06-16 20:52:38 -0300

    Incluir o "Bom dia dia" No começo da impressão de texto no Console
    Criei o método CalcularAreaModoCurto e movi o código que estava em main para lá.
    
    O método man agora chama os dois métodos calcular* que passaram também a ser static

--- src/main/java/intro/Medidas.java
+++ src/main/java/intro/Medidas.java
@@ -9,40 +9,44 @@ public class Medidas {
 
     // 3.2 - Métodos e Funções o que eles sabem fazer
     public static void main(String[] args) {
-        System.out.println("bom dia");
+
+    calcularModoCurto();
+    calcularAreaModoExtenso();
+
+    }
+
+    public static void calcularModoCurto(){
+        System.out.println("Cálculo de área modo curto");
 
         // Calculo de área - Reduzido
         int largura = 4;
         int comprimento = 3;
         System.out.println("Para a largura de " + largura + "m e  o comprimento de "
                 + comprimento + "m A área é de " + largura * comprimento + "m2");
+            }
 
-    }
-      public void calcularAreaModoExtenso(){
 
+      public static void calcularAreaModoExtenso(){
           // Calculo de área - Exemplo: o Tamanho do tapete ou do piso
+
+          System.out.println("Cálculo de área modo Extenso");
 
           int largura;
           int comprimento;
           int resultado;
 
-          largura = 4; //largura recebe 4
-          comprimento = 3;
+          largura = 5; //largura recebe 4
+          comprimento = 6;
 
           resultado = largura * comprimento;
           System.out.println("Para a largura de " + largura + "m e  o comprimento de "
                   + comprimento + "m A área é de " + resultado + "m2");
 
-
-
-
-      }
-       public void calcularAreaModoCompacto(){
-        int largura = 4;
-        int comprimento = 3;
-
-
-           System.out.println("Para a largura de " + largura + "m e  o comprimento de "
-                   + comprimento + "m A área é de " + largura * comprimento + "m2");
-       }
+    }
 }
+
+
+
+
+
+
